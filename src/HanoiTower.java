94f1a14cd23bb1587feb3c801df9672ac2b6845f
Grace Cai <grace.cai7@gmail.com>
2023-08-02 15:58:58 -0700

    fixed and added comments

--- src/HanoiTower.java
+++ src/HanoiTower.java
@@ -6,23 +6,24 @@ public class HanoiTower {
         int numRings;
         while(true) {
             try {
-                System.out.println("How many rings?");
+                // get number of disks
+                System.out.println("How many disks?");
                 numRings = Integer.parseInt(scanner.next());
                 break;
             } catch (Exception e) {
                 System.out.println("Please enter a positive integer");
             }
         }
-        solveHanoi("A", "B", "C", numRings);
+        solveHanoi("A", "B", "C", numRings); // move disks from A to C using B
 
     }
 
     public static void solveHanoi(String first, String middle, String last, int n) {
-        if (n == 0) {
+        if (n == 0) { // terminate when all disks hae been moved from first to last
             return;
         }
-        solveHanoi(first, last, middle, n-1);
-        System.out.println("Move disk " + n + " from " + first + " to " + last);
-        solveHanoi(middle, last, first, n-1);
+        solveHanoi(first, last, middle, n-1); // move disk n-1 from first to middle
+        System.out.println("Move disk " + n + " from " + first + " to " + last); // move disk n from first to last
+        solveHanoi(middle, first, last, n-1); // move disk n-1 from middle to last
     }
 }
